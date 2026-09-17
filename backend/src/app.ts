0f112d099cfef2180fc277a2ba7475f10855e5d0
Sogbanmu Oluwafunmbi <oluwafunmbi10@gmail.com>
2026-09-17 13:27:51 +0100

    fix(web): initialize health endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { corsOptions } from "./lib/corsOption.js";
+import { uptime } from "process";
+import { timeStamp } from "console";
 
 const createApp = function () {
   const app = express();
@@ -10,6 +12,14 @@ const createApp = function () {
   app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.get("/api/health", (req, res)=>{
+    res.status(200).json({
+      status: "UP",
+      timeStamp: new Date(),
+      uptime: process.uptime()
+    })
+  })
 
   return app;
 };
