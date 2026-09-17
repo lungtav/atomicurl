d31622c0ece2b6441265677046e36075e82b473e
Sogbanmu Oluwafunmbi <oluwafunmbi10@gmail.com>
2026-09-17 17:09:07 +0100

    refactor(api): remove old create_url file

--- backend/db/migrations/001.create_url.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto
-CREATE TABLE IF NOT EXISTS urls(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    short_code VARCHAR(7) NOT NULL UNIQUE,
-    original_url TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
