2187d5737d6035caead9f58be00b3e3f184c2821
Sogbanmu Oluwafunmbi <oluwafunmbi10@gmail.com>
2026-09-18 17:00:31 +0100

    feat(api): add get original url service

--- backend/src/services/url.services.ts
+++ backend/src/services/url.services.ts
@@ -1,5 +1,5 @@
 import * as urlRepository from "../repository/url.repository.js";
-import type { createUrlInput } from "../types/url.types.js";
+import type { createUrlInput, getUrlInput } from "../types/url.types.js";
 import { shortCodeGenerator } from "../lib/code.js";
 
 export const createShortUrl = async (input: createUrlInput) => {
@@ -9,4 +9,12 @@ export const createShortUrl = async (input: createUrlInput) => {
 
   const urlRow = await urlRepository.createUrl(url, shortCode);
   return urlRow;
+};
+
+export const getOriginalUrl = async (input: getUrlInput) => {
+  const { shortCode } = input;
+
+  const originalUrl = await urlRepository.getOriginalUrl(shortCode);
+
+  return originalUrl;
 };
