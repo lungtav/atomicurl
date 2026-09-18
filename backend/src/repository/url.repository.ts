de1280d4dce52901140c6acc6621aa24d16840f3
Sogbanmu Oluwafunmbi <oluwafunmbi10@gmail.com>
2026-09-18 19:07:27 +0100

    fix(api): handle collison of short codes

--- backend/src/repository/url.repository.ts
+++ backend/src/repository/url.repository.ts
@@ -1,16 +1,31 @@
 import { db } from "../config/db.js";
+import { ConflictError } from "../errors/ConflictError.js";
+
+type PostgresError = Error & {
+  code?: string;
+};
 
 export const createUrl = async (url: string, shortCode: string) => {
-  const result = await db.query(
-    `
+  try {
+    const result = await db.query(
+      `
         INSERT INTO urls (short_code, original_url)
         VALUES( $1, $2)
         RETURNING short_code, original_url
         `,
-    [shortCode, url],
-  );
+      [shortCode, url],
+    );
 
-  return result.rows[0];
+    return result.rows[0];
+  } catch (error: unknown) {
+    const pgError = error as PostgresError;
+
+    if (pgError.code === "23505") {
+      throw new ConflictError("try creating again");
+    }
+
+    throw error;
+  }
 };
 
 export const getOriginalUrl = async (shortCode: string) => {
