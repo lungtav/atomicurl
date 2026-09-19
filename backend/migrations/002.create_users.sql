bed46fb616b9231a4b0505e2e190d359a47cae44
Sogbanmu Oluwafunmbi <oluwafunmbi10@gmail.com>
2026-09-19 21:25:27 +0100

    feat(api): create user schema

---
+++ backend/migrations/002.create_users.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    clerk_user_id VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
